4f4e4d0aa45b8e5c11aebf718736dc85d31264b8
Mayur <mayur.mistry@perkinswill.com>
2022-12-27 03:11:49 -0800

    DevOps CiCD

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+import gradio as gr
 
 app = FastAPI()
 
+# Gradio
+CUSTOM_PATH = "/gradio"
+io = gr.Interface(lambda x: "Hello, " + x + "!", "textbox", "textbox")
+app = gr.mount_gradio_app(app, io, path=CUSTOM_PATH)
 
+#Welcome
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+#Additonal
 @app.get("/{name}")
 async def welcome(name: str):
     return {"Welcome": name}
+
+
 
